31020ab92f01db727f5c60ac125ebd4a4f6c36aa
ほと <mail@hoto.me>
2015-10-12 14:44:18 +0900

    plumber

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
+import plumber from 'gulp-plumber';
 
 gulp.task('sass', () => {
   gulp.src('./src/styles/index.scss')
+   .pipe(plumber())
    .pipe(sass())
    .pipe(autoprefixer())
    .pipe(gulp.dest('./public'));
