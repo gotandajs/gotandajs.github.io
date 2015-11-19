47235b1bc206a8be0bb7d5ad2ca2ca88fc3589fc
ほと <mail@hoto.me>
2015-11-20 07:53:09 +0900

    人数を取得

--- src/scripts/slack.js
+++ src/scripts/slack.js
@@ -1,7 +1,17 @@
+import request from 'superagent';
+
+const API_BASE = 'https://api.hotolab.net/gotandajs/slack';
+
 let form;
 
 let initialize = function() {
   form = document.querySelector('#slack form');
+
+  request.get(API_BASE).end((err, res) => {
+    if ( err ) { return }
+    document.querySelector('.slack_status .total').textContent = res.body.total;
+    document.querySelector('.slack_status .online').textContent = res.body.online;
+  });
 
   // add event
   form.addEventListener('submit', onSubmit);
@@ -25,15 +35,12 @@ let onSubmit = function(event) {
   form.className = 'wait disable';
   form.email.disabled = true;
 
-  setTimeout(() => {
-    let error = false;
-    if ( error ) {
-      handleError('error');
-    }
-    else {
-      form.className = 'ok disable';
-    }
-  }, 1000);
+  let params = { email: form.email.value };
+  request.post(API_BASE).send(params).end((err, res) => {
+    if ( err ) { return handleError(err) }
+    form.className = 'ok disable';
+    console.log(res.body);
+  });
 };
 
 let handleError = function(msg) {
